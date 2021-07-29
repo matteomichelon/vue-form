e58ae428a85be9adf64490ee145b2490e717c9be
Matteo Michelon <michelon.matteo89@gmail.com>
2021-07-29 16:47:55 +0200

    aggiunto primi stili html css

--- dist/app.js
+++ dist/app.js
@@ -7,30 +7,43 @@
   \********************/
 /***/ (() => {
 
-Vue.use(vuelidate["default"]);
+Vue.use(vuelidate["default"]); // For form_age
+
 var minMaxAge = validators.between(12, 120);
 new Vue({
   el: '#root',
 
   /* DATA */
-  data: function data() {
-    return {
-      form: {
-        name: null,
-        age: null,
-        email: null,
-        newsletter: null
-      }
-    };
+  data: {
+    headerLogo: 'logo.png',
+
+    /* Form Object */
+    form_name: {
+      name: null
+    },
+    form_age: {
+      age: null
+    },
+    form_email: {
+      email: null,
+      newsletter: null
+    },
+
+    /* Form Success */
+    form_name_success: false,
+    form_age_success: false,
+    form_email_success: false
   },
 
   /* VALIDATIONS */
   validations: {
-    form: {
+    form_name: {
       name: {
         required: validators.required // $v.form.name.required
 
-      },
+      }
+    },
+    form_age: {
       age: {
         required: validators.required,
         // $v.form.age.required
@@ -38,11 +51,13 @@ new Vue({
         // $v.form.age.integer
         minMaxAge: minMaxAge // $v.form.age.minMaxAge
 
-      },
+      }
+    },
+    form_email: {
       email: {
         email: validators.email,
         required: validators.requiredIf(function () {
-          return !!this.form.newsletter;
+          return !!this.form_email.newsletter;
         })
       }
     }
@@ -60,13 +75,36 @@ new Vue({
     errorClass: function errorClass(fieldForm) {
       return fieldForm.$error;
     },
-    // Funzione Invia Form
+    // Funzione Invia Form per il nome
     // Entra in funzione quando il bottone invia viene premuto
-    sendForm: function sendForm() {
-      this.$v.form.$touch();
+    sendForm_name: function sendForm_name() {
+      this.$v.form_name.$touch();
 
-      if (!this.$v.form.$invalid) {
-        console.log('📝 Form Submitted', this.form);
+      if (!this.$v.form_name.$invalid) {
+        this.form_name_success = true;
+        console.log('📝 Form Submitted', this.form_name);
+      } else {
+        console.log('❌ Invalid form');
+      }
+    },
+    // Funzione Invia Form per l'età
+    // Entra in funzione quando il bottone invia viene premuto
+    sendForm_age: function sendForm_age() {
+      this.$v.form_age.$touch();
+
+      if (!this.$v.form_age.$invalid) {
+        console.log('📝 Form Submitted', this.form_age);
+      } else {
+        console.log('❌ Invalid form');
+      }
+    },
+    // Funzione Invia Form per la mail
+    // Entra in funzione quando il bottone invia viene premuto
+    sendForm_email: function sendForm_email() {
+      this.$v.form_email.$touch();
+
+      if (!this.$v.form_email.$invalid) {
+        console.log('📝 Form Submitted', this.form_email);
       } else {
         console.log('❌ Invalid form');
       }
--- src/app.js
+++ src/app.js
@@ -1,39 +1,56 @@
 Vue.use( vuelidate.default )
 
+// For form_age
 const minMaxAge = validators.between( 12, 120 )
 
 new Vue( {
     el: '#root',
 
     /* DATA */
-    data () {
-        return {
-            form: {
-                name: null,
-                age: null,
-                email: null,
-                newsletter: null
-            }
-        }
+    data: {
+
+        headerLogo: 'logo.png',
+
+        /* Form Object */
+        form_name: {
+            name: null
+        },
+        form_age: {
+            age: null
+        },
+        form_email: {
+            email: null,
+            newsletter: null
+        },
+
+        /* Form Success */
+        form_name_success: false,
+        form_age_success: false,
+        form_email_success: false,
+
     },
 
     /* VALIDATIONS */
     validations: {
-        form: {
+        form_name: {
             name: {
                 required: validators.required // $v.form.name.required
-            },
+            }
+        },
 
+        form_age: {
             age: {
                 required: validators.required, // $v.form.age.required
                 integer: validators.integer, // $v.form.age.integer
                 minMaxAge // $v.form.age.minMaxAge
-            },
+            }
+        },
 
+        form_email: {
             email: {
                 email: validators.email,
                 required: validators.requiredIf( function () {
-                    return !!this.form.newsletter
+                    return !!this.form_email.newsletter
                 } )
             }
         }
@@ -43,22 +60,45 @@ new Vue( {
     methods: {
         // Funzione per la validazione del campo
         // Deve avere un paramentro. Esempio: $v.form.email
-        validClass ( fieldForm ) { 
+        validClass ( fieldForm ) {
             return !fieldForm.$invalid && fieldForm.$model && fieldForm.$dirty
         },
 
         // Funzione per l'errore del campo
         // Deve avere un paramentro. Esempio: $v.form.email
-        errorClass ( fieldForm ) { 
+        errorClass ( fieldForm ) {
             return fieldForm.$error
         },
 
-        // Funzione Invia Form
+        // Funzione Invia Form per il nome
         // Entra in funzione quando il bottone invia viene premuto
-        sendForm () {
-            this.$v.form.$touch()
-            if ( !this.$v.form.$invalid ) {
-                console.log( '📝 Form Submitted', this.form )
+        sendForm_name () {
+            this.$v.form_name.$touch()
+            if ( !this.$v.form_name.$invalid ) {
+                this.form_name_success = true;
+                console.log( '📝 Form Submitted', this.form_name )
+                
+            } else {
+                console.log( '❌ Invalid form' )
+            }
+        },
+        // Funzione Invia Form per l'età
+        // Entra in funzione quando il bottone invia viene premuto
+        sendForm_age () {
+            this.$v.form_age.$touch()
+            if ( !this.$v.form_age.$invalid ) {
+                console.log( '📝 Form Submitted', this.form_age )
+            } else {
+                console.log( '❌ Invalid form' )
+            }
+        },
+        // Funzione Invia Form per la mail
+        // Entra in funzione quando il bottone invia viene premuto
+        sendForm_email () {
+            this.$v.form_email.$touch()
+            if ( !this.$v.form_email.$invalid ) {
+                console.log( '📝 Form Submitted', this.form_email );
+
             } else {
                 console.log( '❌ Invalid form' )
             }
